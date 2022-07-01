aa3f44b884231e0ff50c8b496404f9453b44ee09
Bezawit11 <socbeza13@gmail.com>
2022-07-01 11:58:29 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  *main - Entry point
  *printing using putchar function
  *Return: Always 0 (Success)
@@ -11,7 +11,7 @@ int x;
 
 for (x = 'a'; x <= 'z'; x++)
 putchar(x);
-for (x = 'A'; x<= 'Z'; x++)
+for (x = 'A'; x <= 'Z'; x++)
 putchar(x);
 putchar('\n');
 return (0);
