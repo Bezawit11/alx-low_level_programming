da502ecaf3ac29e919cd7bcceb331589bfbee9b2
Bezawit11 <socbeza13@gmail.com>
2022-07-03 06:04:41 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,13 @@ int main(void)
 int n, m;
 for (n = 0; n <= 9; n++)
 {
-for (m = 1; m <= 9; m++)
-{
-if (n < m)
+for (m = n+1; m <= 9; m++)
 {
 putchar((n % 10) + '0');
 putchar((m % 10) + '0');
 if (n < 8)
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
