40485aa6459b8195550b81b87a0a190d647e689c
Bezawit11 <socbeza13@gmail.com>
2022-07-05 16:47:56 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,18 @@ int main(void)
 int x;
 unsigned int c = 1;
 unsigned int a = 0;
-for (x = 1; x <= 50; x++)
+for (x = 1; x <= 45; x++)
 {
 c = c + a;
 printf("%d", c);
 printf(",");
 printf(" ");
 a = c - a;
+}
+for (x = 46; x <= 50; x++)
+{
+c = c + a;
+printf("%u", c);
 }
 printf("\n");
 return (0);
