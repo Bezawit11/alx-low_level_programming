cc74ba17a3849052f73a2c51d9d41efaef98502e
Bezawit11 <socbeza13@gmail.com>
2022-07-05 17:18:17 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *prints sum
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+long int c;
+long int a = 0;
+long int s = 0;
+while (c < 4,000,000)
+{
+c = c + a;
+if (c % 2 == 0)
+{
+s += c;
+}
+}
+printf("%ld", s)
+return (0);
+}
