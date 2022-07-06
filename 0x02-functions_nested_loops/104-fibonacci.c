9841c560eaa3345dbbba9bd6cd55900b572dee71
Bezawit11 <socbeza13@gmail.com>
2022-07-06 12:55:50 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- *
- *
+ *main - Entry point
+ *prints 98 Fibonacci numbers
+ *Return: 0
  */
 int main(void)
 
 {
-int c = 1, a = 0;
-int i;
+long int c = 1, a = 0;
+long int i;
 for (i = 1; i <= 98; i++)
 {
 c = c + a;
-printf("%d", c);
+printf("%ld", c);
 printf(",");
 printf(" ");
 a = c - a;
