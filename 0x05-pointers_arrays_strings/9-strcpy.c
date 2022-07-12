7832b5026fb38b235347c43f87f2a4d172d7d679
Bezawit11 <socbeza13@gmail.com>
2022-07-12 01:34:23 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_strcpy - copies pointed string
+ *@dest: pointer
+ *@src: pointer
+ *Return: returns character
+ */
+char *_strcpy(char *dest, char *src)
+
+{
+int l, a;
+l = strlen(src);
+for (a = 0; a < l; a++)
+{
+*(dest + a) = *(src + a);
+}
+return (dest);
+}
