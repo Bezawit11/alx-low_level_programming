ddaf64c71287f641cf240b8901e0535f6e2e2b36
Bezawit11 <socbeza13@gmail.com>
2022-07-11 23:51:41 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *rev_string - a function
+ *@s: pointer
+ *Return: void
+ */
+void rev_string(char *s)
+
+{
+int l, i;
+char str;
+l = strlen(s);
+for (i = 0; i < l/2; i++)
+{
+str = s[i];
+s[i] = s [l - 1 - i];
+s[l - 1 - i] = str;
+}
+}
