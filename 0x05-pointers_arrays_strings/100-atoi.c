7131debf4a54b510eb779e976c60e7e61dc57939
Bezawit11 <socbeza13@gmail.com>
2022-07-12 01:57:16 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int _atoi(char *s)
+
+{
+int l, i;
+l = strlen(s);
+for (i = 0; i <= l; i++)
+{
+if (*(s + i) <= '0' && *(s + i) >= '9')
+break;
+printf("%d", *(s + i));
+}
+return (0);
+}
