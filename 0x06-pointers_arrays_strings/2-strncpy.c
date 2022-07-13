48c7e4ef32b799ce3ad06fce09478bf97ad9cc82
Bezawit11 <socbeza13@gmail.com>
2022-07-13 07:10:16 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = *(src + i);
+}
+return (dest);
+}
