a6193e6fa4d195a3cb9003943a86c5fe4c5bf004
Bezawit11 <socbeza13@gmail.com>
2022-07-13 12:07:51 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,18 +3,23 @@
 #include "main.h"
 
 /**
- *_strncat - a function
+ *_strncat - a function that concatenates
  *@dest: pointer
  *@src: pointer
+ *@n: bytes
  *Return: returns a pointer
  */
 char *_strncat(char *dest, char *src, int n)
 
 {
-int l, i;
+int l, i, d, c;
 l = strlen(dest);
+d = strlen(src);
+c = l + d;
 for (i = 0; i < n; i++)
 {
+if (i > c)
+break;
 *(dest + l + i) = *(src + i);
 }
 return (dest);
