bd4aa0fd6c5a29a175116f7cef3ee18db036364c
Bezawit11 <socbeza13@gmail.com>
2022-07-13 08:11:08 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+
+{
+int l, i, r, a, b;
+l = strlen (s1);
+for (i = 0; i <= l; i++)
+{
+if (*(s1 + i) != *(s2 + i))
+break;
+}
+a = s1[i];
+b = s2[i];
+r = a - b;
+return (r);
+}
