332d68455168910d2b267d39b04b4050eaeb0a85
Bezawit11 <socbeza13@gmail.com>
2022-07-13 15:42:17 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *string_toupper - converts to upper case
+ *@c: char pointer
+ *Return: returns char value
+ */
+char *string_toupper(char *c)
+
+{
+int i, l;
+l = strlen(c);
+for (i = 0; i <= l; i++)
+{
+if (c[i] >= 'a' && c[i] <= 'z')
+c[i] = c[i] - 32;
+}
+return(c);
+}
