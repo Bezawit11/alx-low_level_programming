757611e54afb2fb6d3b684b6f26e947c02577103
Bezawit11 <socbeza13@gmail.com>
2022-07-13 06:34:51 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,12 @@
 char *_strcat(char *dest, char *src)
 
 {
-int i, l;
-l = strlen(src);
-for (i = 0; i < l; i++)
+int i, l1, l2;
+l1 = strlen(src);
+l2 = strlen(dest);
+for (i = 0; i < l1; i++)
 {
-*(dest + i + 6) = *(src + i);
+*(dest + l2 + i) = *(src + i);
 }
 return(dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+
+{
+int l;
+l = strlen(str);
+for
+*(dest 
