665cb050a5353bc432aaf8cd4138d5ed432c399c
Bezawit11 <socbeza13@gmail.com>
2022-07-13 14:19:17 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  *_strcmp - compares a string
  *@s1: character pointer
  *@s2: character pointer
- *Return: return an integer
+ *Return: returns an integer
  */
 int _strcmp(char *s1, char *s2)
 
@@ -16,9 +16,7 @@ l = strlen (s1);
 for (i = 0; i < l; i++)
 {
 if (*(s1 + i) != *(s2 + i))
-{
 break;
-}
 }
 a = s1[i];
 b = s2[i];
