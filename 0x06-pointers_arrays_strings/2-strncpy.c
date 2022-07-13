81bd5ca1b977d2f4b1c0fb2b2558dae0e5ed9558
Bezawit11 <socbeza13@gmail.com>
2022-07-13 12:29:55 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,12 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-int i;
+int i, d, s;
+d = strlen(dest);
+s = strlen(src);
 for (i = 0; i < n; i++) 
 {
-if (dest[i] == '\0')
+if (d < s)
 break;
 *(dest + i) = *(src + i);
 }
