b02661b9d784f9db2688324e5683bde82cda4a75
Bezawit11 <socbeza13@gmail.com>
2022-07-13 12:21:14 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
  *_strncpy - copies a string
  *@dest: pointer
  *@src: pointer
- *n: number of elements
+ *@n: number of elements
  *Return: returns a value
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -14,6 +15,8 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 for (i = 0; i < n; i++) 
 {
+if (dest[i] == '\0')
+break;
 *(dest + i) = *(src + i);
 }
 return (dest);
