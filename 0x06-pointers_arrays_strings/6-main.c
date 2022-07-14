04f720a07f38dca500b864089f75a985f06133ca
Bezawit11 <socbeza13@gmail.com>
2022-07-13 17:45:49 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
 
 /**
  *
@@ -18,7 +15,7 @@ int main(void)
 
 {
 
-	    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	    char str[] = "expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 
 	        char *ptr;
 
