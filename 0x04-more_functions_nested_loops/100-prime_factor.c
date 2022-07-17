32d7565f819d6baffa97e4fc8d12236cc7d29ebd
Bezawit11 <socbeza13@gmail.com>
2022-07-17 06:25:32 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,13 +2,14 @@
 
 /**
  *
+ *  
  *
  */
 int main(void)
 
 {
-long int n, i, a;
-for (n = 2; n <= 1231; n++)
+unsigned long int n, i, a, b;
+for (n = 2; n <= 612852475143; n++)
 {
 for (i = 2; i <= n; i++)
 {
@@ -17,15 +18,14 @@ if (n % i == 0 && n != i)
 break;
 }
 if (n % i != 0)
-continue;
-a = n;
-if (1231 % a == 0)
 {
-printf("%ld", a);
-printf(",");
+continue;
+}
+a = n;
+if (612852475143 % a == 0)
+b = a;
 }
 }
-}
-printf("\n");
+printf("%lu\n", b);
 return (0);
 }
