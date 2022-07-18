8e04a5abff3ce5bd7306f704f625a68084aa5d7a
Bezawit11 <socbeza13@gmail.com>
2022-07-18 02:36:03 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,12 +6,12 @@
  *@dest: pointer
  *@src:pointer
  *@n: int
- *Return: returns a pointer 
+ *Return: returns a pointer
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-int unsigned i;
+unsigned int i;
 for (i = 0; i < n; i++)
 {
 *(dest + i) = *(src + i);
