d9b05c807120ee025a304fd2d18eea5318885e2e
Bezawit11 <socbeza13@gmail.com>
2022-07-18 06:45:49 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 
 {
 unsigned int a = 0;
-int i, j, l1, l2;
+int i, j, l1, l2, b = 0;
 l1 = strlen(accept);
 l2 = strlen(s);
 for (i = 0; i <= l1; i++)
@@ -21,7 +21,12 @@ for (j = 0; j <= l2; j++)
 {
 if (*(s + i) == *(accept + j))
 a = a + 1;
+else
+b = b + 1;
 }
+if (b == l1)
+break;
+b = 0;
 }
 return (a);
 }
