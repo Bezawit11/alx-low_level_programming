e65de75f1c47326ccd47c5ce3b3daa3237df8913
Bezawit11 <socbeza13@gmail.com>
2022-07-18 05:50:45 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_strspn - a function
+ *@s: pointer
+ *@accept: pointer
+ *Return: return a value
+ */
+unsigned int _strspn(char *s, char *accept)
+
+{
+unsigned int a = 0;
+int i, j, l1, l2;
+l1 = strlen(accept);
+l2 = strlen(s);
+for (i = 0; i <= l1; i++)
+{
+for (j = 0; j <= l2; j++)
+{
+if (*(s + i) == *(accept + j))
+a = a + 1;
+}
+}
+return (a);
+}
