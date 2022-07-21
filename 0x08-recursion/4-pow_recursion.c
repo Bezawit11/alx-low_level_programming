2dc072f39d7b471b01f2eb3b6cd8c735ae7678db
Bezawit11 <socbeza13@gmail.com>
2022-07-21 04:09:44 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_pow_recursion - a function
+ *@x: integer
+ *@y: integer
+ *Return: returns a value
+ */
+int _pow_recursion(int x, int y)
+
+{
+int n = x;
+if (y < 0)
+{
+return (-1);
+}
+if (y > 1)
+{
+n = n *_pow_recursion(x, y - 1);
+}
+return(n);
+}
