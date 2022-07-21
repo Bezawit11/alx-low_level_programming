5644cdae8ccdb99b8884c3ca1822a4f01f4c7005
Bezawit11 <socbeza13@gmail.com>
2022-07-21 08:49:24 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int is_prime_number(int n)
+
+{
+return (prime(n, 2));
+}
+
+/**
+ *
+ *
+ */
+int prime(int n, int x)
+
+{
+if ((n != x && n % x == 0) || n <= 1)
+return (0);
+else if (x >= n && n > 1)
+return (1);
+else
+return (prime(n, x + 1));
+}
