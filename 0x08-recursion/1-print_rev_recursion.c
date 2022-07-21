b031adebe5d0a6a899bb29b314313041c966dfba
Bezawit11 <socbeza13@gmail.com>
2022-07-21 03:18:39 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,9 +3,9 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *_print_rev_recursion - a function
+ *@s: a pointer
+ *Return: void
  */
 void _print_rev_recursion(char *s)
 
@@ -15,6 +15,4 @@ if (*s != '\0')
 _print_rev_recursion(s + 1);
 printf("%c", *s);
 }
-else
-printf("\n");
 }
