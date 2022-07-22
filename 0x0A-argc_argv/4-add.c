3904959dfb85860997131ef0d714aa35a898c0be
Bezawit11 <socbeza13@gmail.com>
2022-07-22 12:22:03 -0700

    argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - adds positive numbers
+ *@argc: arg count
+ *@argv: array
+ *Return: returns 1 or 0.
+ */
+int main(int argc, char **argv)
+
+{
+int i, n = 0;
+for (i = 0; i < argc; i++)
+{
+if (atoi(argv[i]) == 0)
+break;
+if (argc == 1)
+return (0);
+n = n + atoi(argv[i]);
+printf("%d", n);
+}
+printf("Error\n");
+return (1);
+}
