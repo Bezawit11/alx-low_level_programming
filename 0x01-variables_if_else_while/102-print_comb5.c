4620f26885f9002fbb149db59bcbc6fa59d7d128
Bezawit11 <socbeza13@gmail.com>
2022-07-23 02:34:04 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,13 @@ for (f = 0; f <= 9; f++)
 {
 for (s = 0; f <= 8; s++)
 {	
+for (t = 0; t <= 9; t++)
+{
+for (z = 1; z <= 9; z++)
+{
 putchar((f % 10) + '0');
 putchar((s % 10) + '0');
 putchar(' ');
-for (t = 0; t <= 9; t++)
-{
-for (z = 1; z <+ 9; z++)
-{
 putchar((t % 10) + '0');
 putchar((z % 10) + '0');
 putchar(',');
