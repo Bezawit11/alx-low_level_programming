a49d31a344e1f3b83c4da7adf569aa6dc6012c1b
Bezawit11 <socbeza13@gmail.com>
2022-07-25 04:47:53 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,9 +17,11 @@ for (i = 0; str[i] != '\0'; i++)
 {
 size = size + 1;
 }
-chr = malloc((size) * sizeof(char) + 1);
+chr = malloc((size) *sizeof(char) + 1);
 if (str == NULL || chr == 0)
-return (0);
+{
+return (NULL);
+}
 else
 {
 for (i = 0; i < size; i++)
