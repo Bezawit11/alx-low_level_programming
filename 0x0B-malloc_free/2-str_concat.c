fd510a0f2b62679b3b782a3cabb980e508e549fa
Bezawit11 <socbeza13@gmail.com>
2022-07-25 07:32:06 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,29 +14,39 @@ char *chr;
 int size1 = 0;
 int size2 = 0;
 int i;
+if (s1 == '\0')
+{
+size1 = 0;
+}
+else
+{
 for (i = 0; s1[i] != '\0'; i++)
 {
 size1 = size1 + 1;
 }
+}
+if (s2 == '\0')
+{
+size2 = 0;
+}
+else
+{
 for (i = 0; s2[i] != '\0'; i++)
 {
 size2 = size2 + 1;
 }
+}
 chr = malloc((size1 + size2) *sizeof(char) + 1);
-if (s1 == NULL)
-s1 = "\0";
-if (s2 == NULL)
-s2 = "\0";
-if (chr == 0)
+if (chr == 1)
+{
 return (0);
+}
 else
 {
 for (i = 0; i < size1; i++)
 chr[i] = s1[i];
 for (i = size1; i < size2 + size1; i++)
-{
 chr[i] = s2[i - size1];
-}
 }
 return (chr);
 }
