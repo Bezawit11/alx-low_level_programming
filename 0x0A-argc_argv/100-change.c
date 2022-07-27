bcef5c82be70069233c7b8ee15fe4af9e656a869
Bezawit11 <socbeza13@gmail.com>
2022-07-27 04:22:42 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,9 +4,9 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *qou - a recrusive function that finds min. no. coins
+ *@n: amount of cents
+ *Return: returns a value int
  */
 int quo(int n)
 
@@ -16,7 +16,7 @@ int c[] = {25, 10, 5, 2, 1};
 for (i = 0; c[i] != '\0'; i++)
 {
 h = c[i];
-if  (n / h > 0)
+if (n / h > 0)
 break;
 }
 a = n / h;
@@ -24,7 +24,7 @@ b = n % h;
 if (b == 25 || b == 10 || b == 5 || b == 2 || b == 1)
 {
 sum = a + 1;
-return(sum);
+return (sum);
 }
 else if (b == 0)
 {
@@ -37,9 +37,10 @@ return (sum);
 }
 }
 /**
- *
- *
- *
+ *main - prints min no. of coins
+ *@argc: argument count
+ *@argv: argument variable
+ *Return: returns 0
  */
 int main(int argc, char **argv)
 
@@ -51,11 +52,12 @@ printf("Error\n");
 return (1);
 }
 a = atoi(argv[1]);
-if (a < 0)
+if (a >= 0)
 {
-printf("0\n");
-}
 s = quo(a);
 printf("%d\n", s);
+}
+else
+printf("0\n");
 return (0);
 }
