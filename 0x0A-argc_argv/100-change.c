7bc32a1fc7ea247bc1bea4b261187546393b6e3c
Bezawit11 <socbeza13@gmail.com>
2022-07-27 02:18:48 -0700

    task5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *
+ *
+ *
+ */
+int remain()
+
+int main(int argc, char **argv)
+
+{
+int c[];
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (argv[1] < 0)
+{
+printf("0\n");
+}
+for (i = 0; i != '\0';i ++)
+{
+if  (argc[1] / c[i] > 0)
+break;
+}
+a = c[i];
+a = argv[1] / c[i];
+b = argv[1] % c[i];
+if (b == c[0] || b == c[1] || b == c[2] || b == c[3] || b == c[4])
+sum = a + b;
+return(sum);
+else
+for (
+
