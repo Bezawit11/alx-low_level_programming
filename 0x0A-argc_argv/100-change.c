c9d32694ad54c115a175ad97ce960290117654fa
Bezawit11 <socbeza13@gmail.com>
2022-07-27 03:14:45 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "main.h"
 
 /**
  *
  *
  *
  */
-int remain()
+int quo(int n)
 
+{
+int i, sum = 0;
+int c[] = {25, 10, 5, 2, 1};
+for (i = 0; c[i] != '\0'; i++)
+{
+if  (n / c[i] > 0)
+break;
+}
+a = argv[1] / c[i];
+b = argv[1] % c[i];
+if (b == c[0] || b == c[1] || b == c[2] || b == c[3] || b == c[4])
+{
+sum = a + b;
+return(sum);
+}
+else if (b == 0)
+{
+return (a);
+}
+else
+{
+sum = a + quo(b);
+return (sum);
+}
+}
+/**
+ *
+ *
+ *
+ */
 int main(int argc, char **argv)
 
 {
-int c[];
+int a, s;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-if (argv[1] < 0)
+a = atoi(argv[1]);
+if (n < 0)
 {
 printf("0\n");
 }
-for (i = 0; i != '\0';i ++)
-{
-if  (argc[1] / c[i] > 0)
-break;
+s = quo(a);
+return (s);
 }
-a = c[i];
-a = argv[1] / c[i];
-b = argv[1] % c[i];
-if (b == c[0] || b == c[1] || b == c[2] || b == c[3] || b == c[4])
-sum = a + b;
-return(sum);
-else
-for (
-
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,1 @@
+int quo(int n);
