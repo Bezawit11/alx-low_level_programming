233c0adc6d7e19599dacc17a56b3fb8d8f81b7f3
Bezawit11 <socbeza13@gmail.com>
2022-07-28 05:05:18 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ *malloc_checked - function
+ *@b: integer
+ *Return: returns void
  */
 void *malloc_checked(unsigned int b)
 
 {
 unsigned int *c = malloc(b);
+if (c == NULL)
+return (98);
+else
 return (c);
 }
