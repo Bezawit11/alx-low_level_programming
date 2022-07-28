12b71c9433ad00afd8adf9e45f41d388fb12af82
Bezawit11 <socbeza13@gmail.com>
2022-07-28 05:45:35 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,3 +8,21 @@
  *
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+char *p = (char *)malloc(sizeof(char) * unsigned int n);
+int i, count1 = 0;
+int count2 = 0;
+for (i = 0; s1[i] = '\0'; i++)
+{
+count1 = count1 + 1;
+}
+for (i = 0; s2[i] = '\0'; i++)
+{
+count2 = count2 + 1;
+}
+for (i = 0; i < count2; i++)
+{
+*(s1 + count1 + i) = *(s2 + i);
+}
+}
