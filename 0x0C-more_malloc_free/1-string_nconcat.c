04076ab5896eadd986a33a96dde3de07be1b5cca
Bezawit11 <socbeza13@gmail.com>
2022-07-28 06:14:05 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,32 +3,30 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *string_nconcat - concatenates strings
+ *@s1: string 1
+ *@s2: string 2
+ *@n: int
+ *Return: returns a pointer to char
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
-char *p;
-int i, count1 = 0;
-int count2 = 0;
-for (i = 0; s1[i] = '\0'; i++)
+char* p;
+unsigned int count1 = 0;
+unsigned int i;
+for (i = 0; s1[i] != '\0'; i++)
 {
 count1 = count1 + 1;
 }
-for (i = 0; s2[i] = '\0'; i++)
-{
-count2 = count2 + 1;
-}
-p = (char *)malloc(sizeof(char) * (n + count1))
+p = (char*)malloc(sizeof(char) * (n + count1));
 for (i = 0; i < count1; i++)
 {
 *(p + i) = *(s1 + i);
 }
-for (i = 0; i < count2; i++)
+for (i = 0; i < n; i++)
 {
-*(p + count1) = *(s2 + i);
+*(p + count1 + i) = *(s2 + i);
 }
 return (p);
 }
