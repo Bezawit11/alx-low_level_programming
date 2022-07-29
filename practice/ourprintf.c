e0a78d169685fbe05927c7c35ce66256be05a6ee
Bezawit11 <socbeza13@gmail.com>
2022-07-29 12:33:33 -0700

    our own printf

--- practice/ourprintf.c
+++ practice/ourprintf.c
@@ -11,6 +11,7 @@ void _printf(char* format, ...)
 {
 char* c;
 int v;
+char* string;
 va_list arg;
 va_start(arg, format);
 for (c = format; *c != '\0'; c++)
@@ -23,7 +24,12 @@ c++;
 if (*c == 'd')
 v = va_arg(arg, int);
 putchar(v);
-if (*c == 'x')
+else if (*c == 'x')
 v = va_arg(arg, 
+putchar
+else if (*c == 's')
+v = va_arg(arg, char*)
+puts(v);
+else if (*c == '
 }
 }
