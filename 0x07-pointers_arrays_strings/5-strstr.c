26e3f3e6afe4bfae6dac166a0aebf6360ea12430
Bezawit11 <socbeza13@gmail.com>
2022-07-30 08:52:02 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,27 +11,26 @@
 char *_strstr(char *haystack, char *needle)
 
 {
-int i, j, l1, l2, a, z;
-l1 = strlen(needle);
-l2 = strlen(haystack);
-for (i = 0; i < l1; i++)
+int i, j, a, z;
+/*l1 = strlen(needle);
+l2 = strlen(haystack);*/
+for (i = 0; needle[i] != '\0'; i++)
 {
-for (j = 0; j < l2; j++)
+for (j = 0; haystack[j] != '\0'; j++)
 {
 if (needle[i] == haystack[j])
 a = j;
-for (z = 1; z < l1; z++)
+for (z = 0; needle[z] != '\0'; z++)
 {
 if (needle[z] != haystack[z + j])
 break;
 }
-if (z == l1)
+if (needle[z] == '\0')
 {
-a = j;
 return (haystack + a);
 }
 }
-if (j == l2)
+if (haystack[j] == '\0')
 break;
 }
 return (0);
