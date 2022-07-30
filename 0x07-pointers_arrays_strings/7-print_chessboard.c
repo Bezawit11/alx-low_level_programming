f094b5dd86d12be966a1e6d9da7e6f92fd4e3db2
Bezawit11 <socbeza13@gmail.com>
2022-07-30 02:09:25 -0700

    chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_chessboard(char (*a)[8])
+
+{
+int i, j;
+char c;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+c = *(*(a + i) + j);
+_putchar(c);
+}
+_putchar('\n');
+}
+}
