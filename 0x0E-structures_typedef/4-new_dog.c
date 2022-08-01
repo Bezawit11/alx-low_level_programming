4ea4104ed06a9aaf9a32e072f556514ab97f0906
Bezawit11 <socbeza13@gmail.com>
2022-08-01 10:56:31 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,9 +15,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *p;
 p = malloc(sizeof(dog_t));
 if (p == NULL)
+{
+free(p);
 return (NULL);
+}
+if (p->name == NULL)
+{
+free(p->name);
+free(p);
+return (NULL);
+}
 p->name = name;
 p->age = age;
+if (p->owner)
+{
+free(p->owner);
+free(p->name);
+free(p);
+return (NULL);
+}
 p->owner =owner;
 return (p);
 }
