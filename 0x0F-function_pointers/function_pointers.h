79ac715ca7004e0d1d7648856b4ec23e51779edc
Bezawit11 <socbeza13@gmail.com>
2022-08-03 07:45:25 -0700

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,6 @@
+#ifndef pointers_file
+#define pointers_file
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
