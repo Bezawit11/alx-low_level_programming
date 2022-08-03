902899dc391614c6cdd10725dafb4b27811cea36
Bezawit11 <socbeza13@gmail.com>
2022-08-03 07:35:24 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
- *
+ *print_name - prints name by calling a fun pointer
+ *@name: string pointer
+ *@f: function pointer
  */
 void print_name(char *name, void (*f)(char *))
 
 {
+if (f != NULL)
 f(name);
 }
