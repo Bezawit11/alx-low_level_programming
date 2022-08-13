07b3348c044a1dd122f63c980f4e2e06e3ae9046
Bezawit11 <socbeza13@gmail.com>
2022-08-13 12:10:32 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,15 @@ size_t print_list(const list_t *h)
 
 {
 size_t l = 0;
+if (h->str == NULL)
+{
+printf("[0] (nil)");
+h = h->next;
+l++;
+}
 while (h != NULL)
 {
-printf("[%d]", h->len);
+printf("[%d] ", h->len);
 printf("%s\n", h->str);
 h = h->next;
 l++;
