ef8a65a002cac32c1323850664ad00d0dc7f7280
Bezawit11 <socbeza13@gmail.com>
2022-08-13 12:15:37 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,14 +4,16 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *print_list - prinys all the elements
+ *@h: node pointer
+ *Return: returns size
  */
 size_t print_list(const list_t *h)
 
 {
 size_t l = 0;
+if (h == NULL)
+return (0);
 if (h->str == NULL)
 {
 printf("[0] (nil)");
