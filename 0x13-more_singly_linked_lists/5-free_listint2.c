c76cc38e90d61f94f4d91b509012b6d24d36458b
Bezawit11 <socbeza13@gmail.com>
2022-08-15 11:20:04 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,12 @@ void free_listint2(listint_t **head)
 
 {
 listint_t *d;
-if (head != NULL)
-{
+if (head == NULL)
+return;
 while (*head != NULL)
 {
 d = *head;
-*head = *(head)->next;
-free(*d);
-}
+*head = (*head)->next;
+free(d);
 }
 }
