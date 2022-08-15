99d311f7e5c6c9bc2b84065142b34b6e6d1fbea5
Bezawit11 <socbeza13@gmail.com>
2022-08-15 09:06:12 -0700

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,8 +4,10 @@
 #include "lists.h"
 
 /**
- *
- *
+ *@add_nodeint_end - adds a new node at the end
+ *@head: head node
+ *@n: data
+ *Return: returns a node struct
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 
