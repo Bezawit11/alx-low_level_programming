78d46d8a9c74f9ae80c67e11cd90bc9ce279e792
Bezawit11 <socbeza13@gmail.com>
2022-08-16 03:02:36 -0700

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -13,7 +13,9 @@ int sum_listint(listint_t *head)
 int i, s = 0;
 listint_t *temp;
 temp = head;
-while (temp->next != NULL)
+if (temp == NULL)
+return (0);
+while (temp != NULL)
 {
 i = temp->n;
 s = s + i;
