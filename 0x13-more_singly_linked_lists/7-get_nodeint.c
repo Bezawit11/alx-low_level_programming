3538e4e0d912dffb920c210edc9e3e6f5e41db36
Bezawit11 <socbeza13@gmail.com>
2022-08-16 02:38:35 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int i;
 listint_t *temp;
 temp = head;
+if (temp == NULL)
+return (NULL);
 for (i = 0; i < index; i++)
 {
 temp = temp->next;
