2e7c57b8b7936f40dbd9f5caa843f3d53f3c9417
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-17 01:02:51 -0700

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,9 +4,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *print_listint - a function that prints all the elements 
+ *@h: node pointer
+ *Return: returns the number of nodes
  */
 size_t print_listint(const listint_t *h)
 
