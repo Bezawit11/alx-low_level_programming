71e566cf735c4e4cc216bc39724774093ca3a059
Bezawit11 <socbeza13@gmail.com>
2022-08-17 02:36:01 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,9 +4,10 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *delete_nodeint_at_index - deletes the node at index
+ *@head: linked list
+ *@index: index of the node that should be deleted
+ *Return: 1 for success -1 for fail
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
