ac4fc0439b65b4be10615830a23c15e33a2aa078
Bezawit11 <socbeza13@gmail.com>
2022-08-17 02:02:21 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,9 +4,11 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *insert_nodeint_at_index - inserts a node
+ *@head: linked list
+ *@idx: index where node is added
+ *@n: data
+ *Return: returns linked list
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
@@ -14,8 +16,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 unsigned int i;
 listint_t *temp, *new;
 temp = *head;
-if (temp == NULL)
-return (NULL);
 for (i = 1; i < idx; i++)
 {
 temp = temp->next;
