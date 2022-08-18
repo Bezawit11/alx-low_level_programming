eeaedf5bced79a8b4ad87481381c9d132e7daed7
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-18 12:25:08 -0700

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,9 +3,10 @@
 #include "main.h"
 
 /**
- *print_binary - prints the binary representation of a number
+ *get_bit -  returns the value of a bit at a given index.
  *@n: decimal to be converted
- *Return: void
+ *@index: index, starting from 0 of the bit you want to get
+ *Return: the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 
