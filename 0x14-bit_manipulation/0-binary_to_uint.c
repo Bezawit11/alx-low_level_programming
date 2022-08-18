d2e12c4c3f27076c5b1b1ff515377684b0763cc8
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-18 03:44:20 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@
 unsigned int binary_to_uint(const char *b)
 
 {
-unsigned int i, d, dec = 0;
+unsigned int i, j, p = 1, d, dec = 0;
 const char *c;
 if (b == NULL)
 return (0);
@@ -23,16 +23,23 @@ b++;
 i = i - 1;
 while (*c != '\0')
 {
+j = i;
 d = *c - '0';
 if (d == 1 || d == 0)
 {
-dec = dec + (d * (2 , i));
+while (j > 0)
+{
+p = p * 2;
+j--;
+}
+dec = dec + (d * p);
 }
 else
 {
 return (0);
 }
 c++;
+i--;
 }
 return (dec);
 }
