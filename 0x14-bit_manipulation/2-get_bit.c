0f35f6b06bf20e057768ad2cd996ad7ccf973e70
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-18 12:21:47 -0700

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,30 @@
-#include 
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *print_binary - prints the binary representation of a number
+ *@n: decimal to be converted
+ *Return: void
+ */
+int get_bit(unsigned long int n, unsigned int index)
+
+{
+unsigned long int r;
+int b;
+char a;
+if (n == 1)
+r = 1;
+if (n > 0)
+{
+r = n % 2;
+get_bit(n / 2, index - 1);
+if (index == 0)
+{
+b = r;
+}
+a = r + '0';
+_putchar(a);
+}
+return (b);
+}
