e5d3b9bc44a907ff0a8b6d3acca7838b334d5363
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-19 13:30:40 -0700

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- *flip_bits - returns the number of bits you would need to flip to get from one number to another
+ *flip_bits - flip bits to get from one number to another
  *@n: number before flipping
  *@m: number after
  *Return: number of bits flipped
@@ -18,4 +18,4 @@ count++;
 h &= (h - 1);
 }
 return (count);
-} 
+}
