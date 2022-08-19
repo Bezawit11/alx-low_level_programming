1475cdbe23165e2821bcc24203b2415f910118f8
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-19 08:25:30 -0700

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,6 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int r, h;
 unsigned int j = 0;
+int p = 1, sum = 0;
 h = *n;
 while (h != 0)
 {
@@ -21,7 +22,6 @@ if (index > j)
 {
 return (-1);
 }
-j = j - index;
 while (n != 0)
 {
 r = n % 2;
@@ -29,6 +29,14 @@ if (index == 0)
 {
 r = 1;
 }
+for (j = j + 0; j > 0; j--)
+{
+p = p * 2;
+}
+sum = sum + (r * p);
 n = n / 2;
 index--;
+j--;
+}
+return (sum);
 }
