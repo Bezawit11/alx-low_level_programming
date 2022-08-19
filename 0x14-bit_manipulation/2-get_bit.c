2c5a0920c6c7a080cee8d5984988baa29bb5b99f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-19 06:11:46 -0700

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,21 +12,19 @@ int get_bit(unsigned long int n, unsigned int index)
 
 {
 unsigned long int r;
-unsigned int m;
-if (n == 0)
+unsigned int h, j = 0;
+h = n;
+while (h != 0)
 {
-r = 1;
-m = 0;
+j = j + 1;
+h = h / 2;
 }
-if (n > 0)
+j = j - 1;
+while (n != 0)
 {
 r = n % 2;
-m++;
-get_bit(n / 2, index);
-if (index == m)
-{
+n = n / 2;
+j--;
+}
 return (r);
-}
-}
-return (-1);
 }
