4598ae4947444c30ce487d1a2a0284992f5644c4
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-26 09:57:07 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
-*
-*
-*/
+ *create_file - creates a new file
+ *@text_content: text to be written
+ *Return: returns 1 on success, -1 if fail
+ */
 int create_file(const char *filename, char *text_content)
  
 {
 int fd, i, h, l = 0;
-if (filename == NULL || text_content == NULL)
+if (filename == NULL)
 return (-1);
+if (text_content == NULL)
+{
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+return (1);
+}
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (fd < 0)
 return (-1);
