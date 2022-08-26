11245c88163f0a34a1211db3c40da88596f923ce
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-26 08:24:15 -0700

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
  
 {
+ssize_t fd, a, b;
+char *let;
 if (filename == NULL)
 return (0);
-open(filename
+fd = open(filename, O_RDONLY);
+if (fd < -1)
+{
+return (0);
+}
+let = malloc(sizeof(char) * letters);
+if (let == NULL)
+return (0);
+a = read
+if (i < 0)
+{
+		free(buf);
+		return (0);
+}
+return (0);
+
 }
