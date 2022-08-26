ba4566355a70f4a2ab16352f85c5678fe5bc49f6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-26 09:51:26 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@ int create_file(const char *filename, char *text_content)
  
 {
 int fd, i, h, l = 0;
-if (text_content == NULL)
+if (filename == NULL || text_content == NULL)
 return (-1);
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (fd < 0)
