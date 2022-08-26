148d151dbff9ecc9dc186ac615d3e96a85489750
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-26 09:47:23 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
 int create_file(const char *filename, char *text_content)
  
 {
-int fd, i, l = 0;
+int fd, i, h, l = 0;
 char *content;
 fd = open(filename, O_CREAT | O_RDWR, 0600);
 if (fd < 0)
@@ -16,8 +16,8 @@ for (i = 0; text_content[l] != '\0'; i++)
 {
 l++;
 }
-x = write(fd, text_content, l);
-if (x < 0)
+h = write(fd, text_content, l);
+if (h < 0)
 return (-1);
 close(fd);
 return (1);
