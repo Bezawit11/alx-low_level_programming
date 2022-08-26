35af81d55ea7a6ff07ee3b938e65a6084b9ed66e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-26 10:40:46 -0700

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,5 +7,20 @@
 int append_text_to_file(const char *filename, char *text_content)
 
 {
- 
+int fd, i, j, l = 0;
+char c;
+fd = open(filename, O_WRONLY);
+if (fd < 0)
+return (-1); 
+for (i = 0; text_content[i] != '\0'; i++)
+{
+l++;
+}
+while (j = read(fd, filename, l) > 0)
+{
+text_content++;
+if (text_content == '\0')
+break;
+}
+write(
 }
