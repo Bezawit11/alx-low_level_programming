a36e948767fe3a4d275e00d541422e19899d46f0
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-26 11:42:39 -0700

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,9 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, i, a, l = 0;
 /*char c;*/
-if (text_content == NULL && filename == NULL)
-return (1);
-if (text_content == NULL)
+if (filename == NULL)
 return (-1);
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd < 0)
