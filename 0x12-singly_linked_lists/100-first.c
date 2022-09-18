2fc2f25518a4fda79c9e6caef520ab5a9619dd08
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-18 01:19:26 -0700

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_b4_main - prints before the main program
+ *Return: void
+ */
+void print_b4_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
