fc90650e550303a37f3cefbf20deeec5be285abb
Bezawit11 <socbeza13@gmail.com>
2022-09-30 06:17:46 -0700

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+
+{
+dlistint_t *new, *c = *head, *g = *head, *p = *head;
+unsigned int i;
+int l = 0;
+if (*head == NULL)
+{
+return -1;
+}
+while (g != NULL)
+{
+l++;
+g = g->next;
+}
+if (index > l)
+return -1;
+if (index == 0)
+{
+c = c->next;
+*head = c;
+return 1;
+}
+for (i = 0; i < index; i++)
+{
+p = c;
+c = c->next;
+}
+p->next = c->next;
+c->next = NULL;
+c->prev = NULL;
+return 1;
+}
