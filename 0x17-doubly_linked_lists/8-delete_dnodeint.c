ef3585291a33b9e4453a18ad03fc0fa089a87041
Bezawit11 <socbeza13@gmail.com>
2022-09-30 06:35:03 -0700

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
  *delete_dnodeint_at_index - a function that deletes the node at index
  *@head: doubly linked list
  *@index: index of the node that should be deleted
- *Returns: 1 if it succeeded, -1 if it failed
+ *Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
