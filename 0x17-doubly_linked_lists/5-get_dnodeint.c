32f6dfe0e58d944b0abc962ddc5294bd1856864c
Bezawit11 <socbeza13@gmail.com>
2022-09-30 02:35:21 -0700

    task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+
+{
+dlistint_t *new = head;
+unsigned int i;
+int h;
+for (i = 0; i < index; i++)
+{
+new = new->next;
+}
+return new;
+}
