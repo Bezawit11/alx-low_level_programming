dd1c0d4896e81d96d648236ed6e88a116dff4ac5
Bezawit11 <socbeza13@gmail.com>
2022-10-03 09:43:56 -0700

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,17 @@
-section .data
-    msg db 'Hello, Holberton', 10
+          global    main
 
-section .text
-    global _start
+          extern    printf
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 16
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+main:
+
+	  mov   edi, format
+
+	  xor   eax, eax
+
+	  call  printf
+
+	  mov 	eax, 0
+
+	  ret
+
+format: db `Hello, Holberton\n`,0
