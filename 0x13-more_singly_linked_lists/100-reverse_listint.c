2e40037352b46715f61712a2339510727368e9ac
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-03 20:12:46 +0100

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -18,3 +18,16 @@ prev = prev->next;
 *head = c;
 return (*head);
 }
+
+listint_t *add_nodeb(listint_t **head, const int n)
+
+{
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->next = *head;
+new->n = n;
+*head = new;
+return (*head);
+}
