c7d8c02058103e2a0ad0429f3f85272cccb2e5d5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-03 17:09:55 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-    msg db "Hello, Holberton",10;
+    msg db "Hello, Holberton",10      ; 10 is the ASCII code for a new line (LF)
 
 section .text
     global _start
@@ -14,13 +14,3 @@ _start:
     mov rax, 60
     mov rdi, 0
     syscall
-
-#include <unistd.h>
-
-#define STDOUT 1
-
-int main()
-{
-    write(STDOUT, "Hello, Holberton\n", 17);
-    _exit(0);
-}
