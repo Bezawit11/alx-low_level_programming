4c0e1e7086cfd0445f4508007b8c33cf4ee18844
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-05 18:42:53 +0100

    Update monty.h

--- practice/monty.h
+++ practice/monty.h
@@ -39,4 +39,4 @@ void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
-void (*instruction(char *str))(stack_t, unsigned int);
+void (*instruction(char *str, int n))(stack_t, unsigned int);
