a690402b81292e6bac8a90ec95e55c4c44931b49
Bezawit11 <socbeza13@gmail.com>
2022-10-05 13:18:09 -0700

    checker

---
+++ practice/checker.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ *
+ */
+int checker(char *str)
+
+{
+int a, i;
+for (i = 0; c[i] != '\0'; c++)
+{
+a = isdigit(*c);
+if (a == 0)
+{
+return 0;
+}
+}
+return 1;
+}
