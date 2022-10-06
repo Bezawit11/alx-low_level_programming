f75e4afd03e5a3185c2f0980d7ee3e4896f5a2bd
Bezawit11 <socbeza13@gmail.com>
2022-10-06 07:27:46 -0700

    helppp

--- practice/0-main.c
+++ practice/0-main.c
@@ -10,11 +10,12 @@
 int main(int argc, char **argv)
 
 {
-int n = 0;/* r;*/
+int n = 0, h;
 stack_t *c = malloc(sizeof(stack_t));
 /*char **opcode;*/
 FILE *ourfile;
-char str[10];
+char s[10];
+char *str = malloc(50 *sizeof(char *));
 /*size_t t;*/
 if (argc != 2)
 {
@@ -27,11 +28,19 @@ printf("Error: Can't open file %s\n", argv[1]);
 exit(EXIT_FAILURE);
 }
 else{
-fgets(str, 10, ourfile);
+fgets(s, 10, ourfile);
 while (str != NULL){
-printf("yayyyyy %s\n", str);
+strcpy(str, s);
+h = strlen(str);
+if (str[h - 1] == '\n')
+{
+str[h - 1] = '\0';
+}
+printf("yayyyyy %s", str);
+printf("woohoo\n");
 instruction(str, n, &c);
-fgets(str, 10, ourfile);
+/*fgets(s, 10, ourfile);*/
+break;
 }
 }
 fclose(ourfile);
