6a09e1b03ea6b9afa4359ba5a74b002848787fe5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-07 09:47:04 +0100

    Update readcmd.c

--- practice/readcmd.c
+++ practice/readcmd.c
@@ -1,1 +1,24 @@
 #include "monty.h"
+
+
+/**
+ *
+ *
+ */
+void read_cmd(char *file)
+  
+{
+FILE *stream;
+stack_t *stack = NULL;
+char *buffer = NULL;
+size_t t = 0;
+stream = fopen(file, "r");
+if (stream == NULL){
+    printf("Error: Can't open file %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+}
+while (getline(&buffer, &t, stream) != -1)
+{
+parse(buffer, line_number);
+}
+}
