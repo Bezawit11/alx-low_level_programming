93b170ce8403e2c616b20d63d79e5fdde7e31db4
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-21 08:27:29 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,12 +6,12 @@
 #include <stdio.h>
 
 /**
- *  * struct hash_node_s - Node of a hash table
- *    * @key: The key, string
- *     * The key is unique in the HashTable
- *      * @value: The value corresponding to a key
- *       * @next: A pointer to the next node of the List
- *        */
+ *struct hash_node_s - Node of a hash table
+ *@key: The key, string
+ *The key is unique in the HashTable
+ *@value: The value corresponding to a key
+ *@next: A pointer to the next node of the List
+ */
 
 typedef struct hash_node_s
 
@@ -23,11 +23,11 @@ struct hash_node_s *next;
 } hash_node_t;
 
 /**
- *  * struct hash_table_s - Hash table data structure
- *    * @size: The size of the array
- *     * @array: An array of size @size
- *      * Each cell of this array is a pointer to the first node of a linked list,
- *       * because we want our HashTable to use a Chaining collision handling
+ *struct hash_table_s - Hash table data structure
+ *@size: The size of the array
+ *@array: An array of size @size
+ *Each cell of this array is a pointer to the first node of a linked list,
+ *because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 
@@ -37,5 +37,5 @@ hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int hash_djb2(const unsigned char *str);
 #endif
