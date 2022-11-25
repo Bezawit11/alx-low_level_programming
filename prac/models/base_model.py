9e003903cc40e8a3c48fd2950096786cd4e725e5
Bezawit11 <socbeza13@gmail.com>
2022-11-24 23:39:13 -0800

    task

--- prac/models/base_model.py
+++ prac/models/base_model.py
@@ -12,11 +12,11 @@ class BaseModel:
                     self.updated_at = datetime.now()
         else:
             
-            self.id = uuid.uuid4()
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
     def __str__(self):
-        return f"{self.__class__.__name__} ({self.id}) <{self.__dict__}>"
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
     def save(self):
         self.updated_at = datetime.now()
     def to_dict(self):
