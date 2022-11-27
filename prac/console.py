ac2aedd9150f0faa8d615ca70a66ea39519b322c
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-27 05:07:55 +0000

    Update console.py

--- prac/console.py
+++ prac/console.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
+"""something"""
+
 
 import cmd
 class HBNBCommand(cmd.Cmd):
-    def __init__(self):
-        cmd.Cmd.__init__(self)
-        self.prompt = '(hbnb) '
+    """i willll"""
+    prompt = "(hbnb) "
+        
+    def do_help(self, arg):
+        """To get help
+        """
+        return super().do_help(arg)
+
+    def do_EOF(self, line):
+        """  """
+        print("")
+        return True
 
     def do_quit(self, arg):
+        """  """
         return True
-    
     def help_quit(self):
+        """  """
         print("Quit command to exit the program")
-        
-    def do_EOF(self, arg):
-        return True
-        
+        print()
     def help_EOF(self):
-        print("terminate")
+        """ """
+        pass
+    def emptyline(self):
+        """    """
+        pass
         
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
