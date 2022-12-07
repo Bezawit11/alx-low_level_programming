7a746f8934b844d0e9d5a3ea659b792ada940726
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-07 19:16:58 +0000

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""  """
+"""island_perimeter"""
+
 
 def island_perimeter(grid):
-    """   """
+    """returns the perimeter of the island described in grid"""
     sum = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
