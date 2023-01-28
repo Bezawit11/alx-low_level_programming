2000d798fc60d2937e8fa685ed988c939fcd2af2
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-28 12:38:28 +0000

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 size_t i;
 for (i = 0; i < size; i++)
 {
-printf("Value checked array[%d] = [%d]\n", i, array[i]);
+printf("Value checked array[%d] = [%ld]\n", i, array[i]);
 if (array[i] == value)
 return i;
 }
