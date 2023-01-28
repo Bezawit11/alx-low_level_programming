fce26c4098e3f50df7393d70fb8d9a60826a7c74
Bezawit11 <socbeza13@gmail.com>
2023-01-28 05:40:26 -0800

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,8 +10,10 @@
 int binary_search(int *array, size_t size, int value)
 {
 int a;
+if (array == NULL)
+return (-1);
 a = search(array, 0, size - 1, value);
-return a;
+return (a);
 }
 
 /**
@@ -21,17 +23,18 @@ return a;
  */
 int search(int *array, size_t low, size_t high, int value)
 {
-size_t a = (high + low) / 2;
-if (low == high){
-return -1;
+size_t a = (high + low) / 2, r;
+if (low == high)
+return (-1);
+if (array[a] == value)
+return (a);
+else if (array[a] > value)
+{
+r = search(array, low, a - 1, value);
 }
-if (array[a] == value){
-return a;
+else
+{
+r = search(array, a + 1, high, value);
 }
-else if (array[a] > value){
-search(array, low, a - 1, value);
-}
-else{
-search(array, a + 1, high, value);
-}
+return (r);
 }
