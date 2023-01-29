f96dd28fd3f8b1f7778351e9fb783c3d02f5f0e7
Bezawit11 <socbeza13@gmail.com>
2023-01-29 08:44:29 -0800

    jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define SEARCH_H
 
 int linear_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int search(int *array, size_t low, size_t high, int value);
 #endif
