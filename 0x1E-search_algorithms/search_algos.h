4c354b88ab5d6c341b05525db830554b2fc93886
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-30 20:30:44 +0000

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,6 @@ int linear_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int search(int *array, size_t low, size_t high, int value);
+int i_Search(int *array, int low, int high, int x);
+int interpolation_search(int *array, size_t size, int value);
 #endif
